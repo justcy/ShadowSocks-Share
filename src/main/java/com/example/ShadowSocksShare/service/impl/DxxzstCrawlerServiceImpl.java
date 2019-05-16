a2bfc9b4378b815b5bb1f73c6a5f48a4bd3dac68
chenyu <chenyu@2xi.com>
2019-05-16 18:44:27 +0800

    增加dxxzst

---
+++ src/main/java/com/example/ShadowSocksShare/service/impl/DxxzstCrawlerServiceImpl.java
@@ -0,0 +1,94 @@
+package com.example.ShadowSocksShare.service.impl;
+
+import com.example.ShadowSocksShare.domain.ShadowSocksDetailsEntity;
+import com.example.ShadowSocksShare.service.ShadowSocksCrawlerService;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * https://github.com/dxxzst/Free-SS-SSR
+ */
+@Slf4j
+@Service
+public class DxxzstCrawlerServiceImpl extends ShadowSocksCrawlerService {
+
+	// 目标网站 URL
+	private static final String TARGET_URL = "https://github.com/dxxzst/Free-SS-SSR";
+
+	// 访问目标网站，是否启动代理
+	@Value("${proxy.enable}")
+	@Getter
+	private boolean proxyEnable;
+	// 代理地址
+	@Getter
+	@Value("${proxy.host}")
+	private String proxyHost;
+	// 代理端口
+	@Getter
+	@Value("${proxy.port}")
+	private int proxyPort;
+
+
+	// 解析 连接方式
+	@Override
+	protected Set<ShadowSocksDetailsEntity> parse(Document document) {
+		Elements ssList = document.select("td");
+		// SSR 订阅地址内容
+		String htmlSrc = document.text();
+		String pattern = "ssr:(.*?)";
+		Matcher m = Pattern.compile(pattern).matcher(htmlSrc);
+		String[] ssrLinkList;
+		Set<String> ssrLinkSet = new HashSet<>(m.groupCount());
+		while (m.find()){
+			ssrLinkSet.add("ssr:"+m.group());
+		}
+		// log.debug("---------------->{}={}", ssrLinkList.length + "", ssrLinkList);
+		Set<ShadowSocksDetailsEntity> set = Collections.synchronizedSet(new HashSet<>(ssrLinkSet.size()));
+
+		for (String str : ssrLinkSet) {
+			try {
+				if (StringUtils.isNotBlank(str)) {
+					ShadowSocksDetailsEntity ss = parseLink(str.trim());
+					ss.setValid(false);
+					ss.setValidTime(new Date());
+					ss.setTitle("免费账号 | 云端框架");
+					ss.setRemarks("Dxxzst");
+					ss.setGroup("Dxxzst");
+
+					// 测试网络
+					if (isReachable(ss))
+						ss.setValid(true);
+
+					// 无论是否可用都入库
+					set.add(ss);
+
+					log.debug("*************** 第 {} 条 ***************{}{}", set.size(), System.lineSeparator(), ss);
+					// log.debug("{}", ss.getLink());
+				}
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+
+		return set;
+	}
+
+	/**
+	 * 目标网站 URL
+	 */
+	@Override
+	protected String getTargetURL() {
+		return TARGET_URL;
+	}
+}
